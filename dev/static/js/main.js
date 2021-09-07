a122e6b1c31da37a6763fbbc1424ee558cda9d96
Artur Ballod <artur.ballod@gmail.com>
2021-09-08 01:11:11 +0200

    #beta-first

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,9 +1,45 @@
-var menuOpener = document.querySelector('.js-toggle');
-var menu = document.querySelector('.nav');
-var menuLayer = document.querySelector('.body');
+var firstStep = document.querySelector('.js-start');
+var firstPage = document.querySelector('.header');
+var secondPage = document.querySelector('.content');
+var secondStep = document.querySelector('.js-activate');
+var submitBtn = document.querySelector('.js-submit');
+var thirdPage = document.querySelector('.event');
+var lastPage = document.querySelector('.footer');
 
-menuOpener.addEventListener('click', function() {
-    menu.classList.toggle('active');
-    menuOpener.classList.toggle('active');
-    menuLayer.classList.toggle('no-scroll');
+firstStep.addEventListener('click', function() {
+    firstPage.classList.add('passed');
 });
+
+if (secondStep) {
+    secondStep.addEventListener('click', function() {
+        firstPage.classList.add('passed-2');
+        secondPage.classList.add('passed');
+        setTimeout(function() {
+            return secondPage.classList.add('invisible');
+        }, 1000);
+
+        setTimeout(function() {
+            return thirdPage.classList.add('visible');
+        }, 500);
+    });
+};
+
+
+if (submitBtn) {
+    submitBtn.addEventListener('click', function() {
+        event.preventDefault();
+        thirdPage.classList.add('passed');
+
+        setTimeout(function() {
+            return thirdPage.classList.add('invisible');
+        }, 500);
+
+        setTimeout(function() {
+            return lastPage.classList.add('visible');
+        }, 500);
+
+        setTimeout(function() {
+            return lastPage.classList.add('active');
+        }, 1000);
+    });
+};
